736b499ae270a7b3b1304d9145eb748c9c1327f3
Lev Kokotov <lev.kokotov@instacart.com>
2020-11-06 14:48:01 -0800

    different function

--- src/logging.c
+++ src/logging.c
@@ -142,7 +142,7 @@ void log_pkt_to_buffer(PktHdr *pkt, PgSocket *client) {
   }
 
   log_info("Client id: %u", (uint32_t)client->client_id);
-  memcpy(buf + len, (char*)&client->client_id, sizeof(uint32_t));
+  snprintf(buf + len, sizeof(uint32_t), "%u", client->client_id);
   log_info("Buffer: %u", (uint32_t)*(buf + len));
   len += sizeof(uint32_t);
 

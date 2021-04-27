42ae7c98e7eee837a14887c8bde2adddde0a88bc
Zain Kabani <zain.kabani@instacart.com>
2021-04-27 12:01:03 -0400

    Adds skipped packet logging

--- include/logging.h
+++ include/logging.h
@@ -16,5 +16,6 @@
 
 void log_setup(void);
 void log_connect_to_buffer(bool connected, PgSocket *client);
+void log_pkt_skipped_to_buffer(PktHdr *pkt, PgSocket *client);
 void log_pkt_to_buffer(PktHdr *pkt, PgSocket *client);
 void log_reload_to_buffer(void);

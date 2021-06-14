acf4b9892066d1e9964776fe8916e8e0a5e8d6e8
Felipe Andrade <felipe.andrade@instacart.com>
2021-06-14 11:29:11 -0700

    bump log_buffer_size to 4mb

--- src/logging.c
+++ src/logging.c
@@ -24,7 +24,7 @@
 #include <sys/file.h>
 #include <time.h>
 
-#define LOG_BUFFER_SIZE 1024 * 1024 * 2 /* 2 MB */
+#define LOG_BUFFER_SIZE 1024 * 1024 * 4 /* 4 MB */
 
 /* File id to prevent accidental collision, appended to file name such as 'pktlog.001' */
 /*
